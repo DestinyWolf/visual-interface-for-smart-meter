ab454e3edf0efd662b55c428186825428f5dd32e
Maike de Oliveira Nascimento <64764136+DestinyWolf@users.noreply.github.com>
2024-05-17 17:00:49 -0300

    fix: correção do label de data
    alinhamento do campo de data com o meio da tela

--- src/app/graficosPorTempo/porDia/page.tsx
+++ src/app/graficosPorTempo/porDia/page.tsx
@@ -5,8 +5,11 @@ export default function Home() {
 
     return (
       <main className="flex-col min-h-screen p-24">
+        <div className="items-center justify-center text-center">
+          <DataSelector/>
+        </div>
+        
         <div className=" flex-col border-2 border-black  justify-center items-center">
-            <DataSelector/>
             <GraficoCorrente/>
         </div>
       </main>
